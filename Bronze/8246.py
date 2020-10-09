fa6ad619776bd9abfb6c193cb7908d81e81da493
SnapFlip20 <jungmin1755@gmail.com>
2020-10-09 23:40:40 +0900

    Stół

---
+++ Bronze/8246.py
@@ -0,0 +1,12 @@
+'''
+8246 - Stół
+(이 문제는 구글 번역을 사용하여 해결하였기 때문에 정해가 아닐 수도 있음)
+'''
+
+import sys
+
+a = int(sys.stdin.readline())
+b = int(sys.stdin.readline())
+
+print((a - b) // 2 + b)
+print((a - b) // 2)
