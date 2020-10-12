49ecf9ae04ebcb5b377b3f98e64cd6e50f4d97ac
SnapFlip20 <jungmin1755@gmail.com>
2020-10-13 00:23:10 +0900

    Beatiful Yekaterinburg

---
+++ Bronze/9524.py
@@ -0,0 +1,16 @@
+'''
+9524 - Beautiful Yekaterinburg
+'''
+
+import sys
+
+n = int(sys.stdin.readline())
+
+if n == 1:
+    print(1)
+elif n == 2:
+    print(7)
+elif n == 3:
+    print(2)
+elif n == 4:
+    print(3)
